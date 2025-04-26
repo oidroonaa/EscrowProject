1b9dcdf416a0055126752154862d278babb865b8
oidroonaa <159604482+oidroonaa@users.noreply.github.com>
2025-04-26 21:44:43 +0800

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+// hardhat.config.js
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
+
