26486c6f6ceaae351844994fe92c5b21ca5e5cb4
oidroonaa <159604482+oidroonaa@users.noreply.github.com>
2025-05-02 16:17:32 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,13 @@ const hre = require("hardhat");
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-
+  console.log("Deploying contracts with the account:", deployer.address);
 
   const Escrow = await hre.ethers.getContractFactory("FreelanceEscrow");
-  const escrow = await Escrow.deploy();
+  const escrow = await Escrow.deploy({
+    gasPrice: ethers.utils.parseUnits("10", "gwei"),
+    gasLimit: 5_000_000,
+  });
   await escrow.deployed();
   console.log("Contract deployed to:", escrow.address);
   
