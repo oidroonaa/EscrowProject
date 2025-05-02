9400630458039110750815ea174f6f303f98d350
oidroonaa <159604482+oidroonaa@users.noreply.github.com>
2025-05-02 16:19:03 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
 
 module.exports = {
   solidity: "0.8.20",
@@ -9,10 +10,21 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env.PRIVATE_KEY].filter(Boolean),
+      gasPrice: 10_000_000_000,
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
 };
+
 
